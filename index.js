d7b8bcfacb37ea2fcbb47c71bc156cb509175f59
Your Name <russelgood68@gmail.com>
2025-06-11 08:06:53 +0800

    minor updates

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const nextBtn = document.getElementById("nextBtn");
 
     let currentIndex = 0;
-    const cardWidth = 320;
+    const slideGap = 20;
+    const items = cardContainer.children.length;
+    const cardWidth = 320 + slideGap;
+
 
     nextBtn.addEventListener("click", function () {
         const maxIndex = cardContainer.children.length - 1;
         if (currentIndex < maxIndex) {
-            currentIndex++;
+            currentIndex += items;
             cardContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
         }
     });
 
     prevBtn.addEventListener("click", function () {
         if (currentIndex > 0) {
-            currentIndex--;
+            currentIndex -= items;
             cardContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
         }
     });
