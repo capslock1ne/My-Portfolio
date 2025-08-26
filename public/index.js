e7e22952845f99815083b66c7364608c55ddf6bc
Your Name <russelgood68@gmail.com>
2025-08-26 22:55:38 +0800

    minor

--- public/index.js
+++ public/index.js
@@ -1,28 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const cardContainer = document.getElementById("cardContainer");
-    const prevBtn = document.getElementById("prevBtn");
-    const nextBtn = document.getElementById("nextBtn");
+  const cardContainer = document.getElementById("cardContainer");
+  const prevBtn = document.getElementById("prevBtn");
+  const nextBtn = document.getElementById("nextBtn");
 
-    let currentIndex = 0;
-    const slideGap = 20;
-    const items = cardContainer.children.length;
-    const cardWidth = 320 + slideGap;
+  let currentIndex = 0;
+  const slideGap = 20;
+  const cardWidth = 320 + slideGap;
+  const totalItems = cardContainer.children.length;
 
+  const step = 1; 
 
-    nextBtn.addEventListener("click", function () {
-        const maxIndex = cardContainer.children.length - 1;
-        if (currentIndex < maxIndex) {
-            currentIndex += items;
-            cardContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-        }
-    });
+  nextBtn.addEventListener("click", function () {
+    const maxIndex = totalItems - step;
+    if (currentIndex < maxIndex) {
+      currentIndex += step;
+      cardContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+    }
+  });
 
-    prevBtn.addEventListener("click", function () {
-        if (currentIndex > 0) {
-            currentIndex -= items;
-            cardContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-        }
-    });
+  prevBtn.addEventListener("click", function () {
+    if (currentIndex > 0) {
+      currentIndex -= step;
+      cardContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+    }
+  });
 });
 
 
